e38d42757e9973e4f3394958f49a239e38e2e246
Ashasubashini <ashasubashini2005@gmail.com>
2025-03-14 12:11:09 +0530

    add server url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5002/api/',
+    baseURL: 'https://chat-app-server-lemon.vercel.app/api/',
     withCredentials: true,
     
 });
