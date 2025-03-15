bb68d120d91ce9b16b7785875ef439018847133f
Asha Subhashini <144410722+Ashasubashini@users.noreply.github.com>
2025-03-15 22:42:00 +0530

    Update auth.middleware.js

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -4,6 +4,9 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
     try {
+        res.header('Access-Control-Allow-Origin', process.env.ORIGIN);
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         const token = req.cookies.jwt; 
 
         if (!token) {
