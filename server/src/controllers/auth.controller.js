d5e9a032ae132510d7028f53f01392be9c5b43a7
Ashasubashini <ashasubashini2005@gmail.com>
2025-03-15 18:40:59 +0530

    last update

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -13,7 +13,7 @@ export const signup = async (req, res) => {
         }
         //hash password
         if (password.length < 6) {
-            return res.status(400).json({message: "Password must be at least 8 characters long"});
+            return res.status(400).json({message: "Password must be at least 6 characters long"});
         }
         const user = await User.findOne({email})
         if (user) 
