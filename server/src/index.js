707d53ef4651fcb5fe71e44b8d0bcb05d069765e
Ashasubashini <ashasubashini2005@gmail.com>
2025-03-15 18:57:23 +0530

    updating cors

--- server/src/index.js
+++ server/src/index.js
@@ -12,10 +12,20 @@ import {app, server} from './lib/socket.js';
 const PORT = process.env.PORT || 5002;
 
 app.use(cors({
-    origin: process.env.ORIGIN,
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            "https://chat-app-client-five-ruddy.vercel.app",
+            "http://localhost:5173" // Allow localhost for development
+        ];
+
+        if (!origin || allowedOrigins.includes(origin.replace(/\/$/, ""))) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
-
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
